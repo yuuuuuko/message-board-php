551b6cf1a31de447d1aaa6b8dbf16cd4f8575d90
yuuko.kitamura <phyu10078@gmail.com>
2021-01-27 12:50:28 +0000

    CRUD messages

---
+++ resources/views/messages/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>メッセージ一覧</h1>
+
+    @if (count($messages) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($messages as $message)
+                <tr>
+                    {{-- メッセージ詳細ページへのリンク --}}
+                    <td>{!! link_to_route('messages.show', $message->id, ['message' => $message->id]) !!}</td>
+                    <td>{{ $message->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    
+    {{-- メッセージ作成ページへのリンク --}}
+    {!! link_to_route('messages.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
+
+
+@endsection
